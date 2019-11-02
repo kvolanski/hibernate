211cd4fa23c572c3d795f53404504cd3e3782405
Kevin Volanski <kvolanski@gmail.com>
2019-11-01 23:25:09 -0300

    Add files via upload

---
+++ src/main/java/br/com/papayaSugar/entity/Main.java
@@ -0,0 +1,17 @@
+package br.com.papayaSugar.entity;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    public static void main(String[] args) {
+
+        entityManagerFactory = Persistence.createEntityManagerFactory("hibernatejpa");
+
+    }
+
+}
